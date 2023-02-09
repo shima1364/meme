bda79c0dc61026fb5e753b16a5e8cb05b6ba5012
shima1364 <sh.movafagh.474@gmail.com>
2023-02-09 19:25:24 +0100

    first commit

---
+++ src/components/MemeGenerator.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+
+const MemeGenerator = () => {
+  const [inputText, setInputText] = useState({
+    topText: "",
+    bottomText: ""
+  });
+  const [randomImage, setRandomImage] = useState(
+    "https://i.imgflip.com\/1g8my4.jpg"
+  );
+  const [allMemeImgs, setAllMemeImgs] = useState([]);
+
+  const handleChange = e => {
+    setInputText({
+      ...inputText,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const randNum = Math.floor(Math.random() * allMemeImgs.length);
+    const randMemeImgUrl = allMemeImgs[randNum].url;
+    setRandomImage(randMemeImgUrl);
+  };
+
+  useEffect(() => {
+    console.log("test run");
+    fetch("https://api.imgflip.com/get_memes")
+      .then(response => response.json())
+      .then(response => setAllMemeImgs(response.data.memes));
+  }, []);
+  const backgroundImage = {
+    backgroundImage: `url(${randomImage})`}
+
+  return (
+    <div className="meme-container">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="topText"
+          placeholder="Add Top Text"
+          value={inputText.topText}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="bottomText"
+          placeholder="Add Bottom Text"
+          value={inputText.bottomText}
+          onChange={handleChange}
+        />
+        <button>Generate</button>
+      </form>
+      <div className="meme">
+        <img src={randomImage}/>
+        <h2 className="top">{inputText.topText}</h2>
+        <h2 className="bottom">{inputText.bottomText}</h2>
+      </div>
+    </div>
+  );
+};
+
+export default MemeGenerator;
+
